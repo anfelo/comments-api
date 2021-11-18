cfa0880bf2d9947a549014b865690f4f62e84574
anfelo <af.osorio1341@gmail.com>
2021-11-18 17:31:18 -0500

    Structured the application entry point

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+// App - the struct which contains things like pointers
+// to database connections
+type App struct{}
+
+// Run - sets up our application
+func (a *App) Run() error {
+	fmt.Println("Setting Up Out APP")
+	return nil
+}
+
 func main() {
 	fmt.Println("Go Rest API Course")
+	app := App{}
+
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up our REST API")
+		fmt.Println(err)
+	}
 }
